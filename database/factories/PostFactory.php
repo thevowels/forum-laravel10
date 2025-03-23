941576bb9aab86126b69ac7153bc5aebbb7013c5
thevowels <aungphyohtet1@gmail.com>
2025-03-24 05:02:40 +0700

    Database Seeders

--- database/factories/PostFactory.php
+++ database/factories/PostFactory.php
@@ -2,6 +2,9 @@
 
 namespace Database\Factories;
 
+//use App\Models\Post;
+use App\Models\User;
+
 use Illuminate\Database\Eloquent\Factories\Factory;
 
 /**
@@ -18,6 +21,10 @@ class PostFactory extends Factory
     {
         return [
             //
+            'user_id' => User::factory(),
+            'title' => str(fake()->sentence())->beforeLast('.')->title(),
+            'body' => fake()->realText(600),
+
         ];
     }
 }
